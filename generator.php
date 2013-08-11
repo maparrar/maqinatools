ed9f3727e859f500c0ca49f574300218f3043f19
maparrar <maparrar@gmail.com>
2013-08-11 02:24:52 -0500

    Inicialización del repositorio

---
+++ generator.php
@@ -0,0 +1,358 @@
+
+
+
+
+<?php
+    $package="models";
+    $subpackage="social";
+    $class="Test";
+    $attributes=array(
+        array("name"=>"id",         "type"=>"int",      "comment"=>"Suggestion id"),
+        array("name"=>"content",    "type"=>"string",   "comment"=>"Suggestion content"),
+        array("name"=>"page",       "type"=>"string",   "comment"=>"Página en la que debe aparecer [home|profile|folio|...]"),
+        array("name"=>"element",    "type"=>"string",   "comment"=>"Elemento de la página al que estará referenciado [.divClase|#divConId|...]"),
+        array("name"=>"position",   "type"=>"string",   "comment"=>"Posición respecto al elemento [north|south|east|west]"),
+        array("name"=>"arrowPosition","type"=>"int",    "comment"=>"Posición de la flecha en el lado especificado, si es north, la \$arrowPosition=0
+        *    indica que está a cero pixeles de la parte izquierda de abajo, si \$arrowPosition=100
+        *    indica que está a 100 pixeles de la parte izquierda de abajo"),
+        array("name"=>"height",     "type"=>"int",      "comment"=>"Alto de la sugerencia en pixeles, si es 0 se calculará automáticamente"),
+        array("name"=>"width",      "type"=>"int",      "comment"=>"Ancho de la sugerencia en pixeles, si es 0 se calculará automáticamente"),
+        array("name"=>"image",      "type"=>"string",   "comment"=>"Si tiene alguna imagen, almacena la ruta respecto a la carpteta data")
+    );
+    
+    
+    //Genera la clase
+    classGenerator($package,$subpackage,$class,$attributes);
+    //Genera el DAO para la clase
+    daoGenerator($package,$subpackage,$class,$attributes);
+    
+    
+/******************************************************************************/
+/***************************** GENERADOR DE CLASE *****************************/
+/******************************************************************************/
+function classGenerator($package,$subpackage,$class,$attributes){
+$file = 'classes/'.$class.'.php';
+$header='<?php
+/** '.$class.' File
+ * @'.$package.' models @'.$subpackage.' social */
+include_once Router::rel(\'models\').\'core/Object.php\';
+/**
+ * '.$class.' Class
+ * 
+ * @author bonfolio.co
+ * @author Alejandro Parra <alejandro.parra@bonfolio.co> - @author Juan Cárdenas <juan.cardenas@bonfolio.co>
+ * @package '.$package.'
+ * @subpackage '.$subpackage.'
+ */';
+/******************************* ATRIBUTOS ********************************/
+foreach ($attributes as $attribute){
+    $atributes.='
+    /** '.$attribute['comment'].' 
+     * @var '.$attribute['type'].'
+     */
+    protected $'.$attribute['name'].';';
+}
+/****************************** CONSTRUCTOR *******************************/
+$constructor='
+    /**
+    * Constructor';
+foreach ($attributes as $attribute){
+    $value='0';
+    if(strtolower($attribute['type'])==="string"){
+        $value='""';
+    }elseif(strtolower($attribute['type'])==="date"){
+        $value='date(\'Y-m-d H:i:s\')';
+    }
+    $parameters.='$'.$attribute['name'].'='.$value.',';
+    $constructor.='
+    * @param '.$attribute['type'].' '.$attribute['comment'].'        ';
+}
+$parameters=substr($parameters,0,-1);
+$constructor.='
+    */';
+$constructor.='
+    function '.$class.'('.$parameters.'){        ';
+foreach ($attributes as $attribute){
+    $constructor.='
+        $this->'.$attribute['name'].'=$'.$attribute['name'].';'; 
+}
+$constructor.='
+    }';
+/******************************** SETTERS *********************************/
+$setters='
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>   SETTERS   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<';
+foreach ($attributes as $attribute){
+    $setters.='
+    /**
+    * Setter '.$attribute['name'].'
+    * @param '.$attribute['type'].' $value '.$attribute['comment'].'
+    * @return void
+    */
+    public function set'.ucfirst($attribute['name']).'($value) {
+        $this->'.$attribute['name'].'=$value;
+    }        '; 
+}
+/******************************** SETTERS *********************************/
+$getters='
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>   SETTERS   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<';
+foreach ($attributes as $attribute){
+    $getters.='
+    /**
+    * Getter: '.$attribute['name'].'
+    * @return '.$attribute['type'].'
+    */
+    public function get'.ucfirst($attribute['name']).'() {
+        return $this->'.$attribute['name'].';
+    }        '; 
+}
+/********************************* CLASS **********************************/
+$class='
+class '.$class.' extends Object{';
+$class.=$atributes;
+$class.=$constructor;
+$class.=$setters;
+$class.=$getters;
+$class.='    
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>   METHODS   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+}
+?>';
+//Escribe la clase
+file_put_contents($file,$header.$class);
+print_r("CLASE GENERADA <br>");
+}
+
+
+
+/******************************************************************************/
+/**************************** GENERADOR DE MODELO *****************************/
+/******************************************************************************/
+function daoGenerator($package,$subpackage,$class,$attributes){
+$file = 'classes/Dao'.$class.'.php';
+$header='<?php
+/** Dao'.$class.' File
+ * @package models @subpackage dal */
+if(!class_exists(\'Router\')) require_once \'../../config/Router.php\';
+include_once Router::rel(\'models\').\'dal/Dao.php\';
+include_once Router::rel(\'models\').\''.$subpackage.'/'.$class.'.php\';
+/**
+ * Dao'.$class.' Class
+ *
+ * Class data layer for the '.$class.' class
+ * 
+ * @author bonfolio.co
+ * @author Alejandro Parra <alejandro.parra@bonfolio.co> - @author Juan Cárdenas <juan.cardenas@bonfolio.co>
+ * @package models
+ * @subpackage dal
+ */';
+
+/****************************** CONSTRUCTOR *******************************/
+$constructor='
+    /**
+     * Constructor: sets the database Object and the PDO handler
+     * @param string Type of connection string to use
+     */
+    function Dao'.$class.'(){
+        parent::Dao();
+    }';
+/*********************************** CREATE ***********************************/
+    $create='
+    /**
+     * Create an '.$class.' in the database
+     * @param '.$class.' new '.$class.'
+     * @return '.$class.' '.$class.' stored
+     * @return string "exist" if the '.$class.' already exist
+     * @return false if the '.$class.' couldn\'t created
+     */
+    function create($'.strtolower($class).'){
+        $created=false;
+        if(!$this->exist($'.strtolower($class).'->getId())){    ';
+    foreach ($attributes as $attribute){
+        $columns.='`'.$attribute['name'].'`,';
+        $parameters.=':'.$attribute['name'].',';
+    }
+    $columns=substr($columns,0,-1);
+    $parameters=substr($parameters,0,-1);
+    $sql='"INSERT INTO '.strtolower($class).'s 
+                (';
+    $sql.=$columns.') VALUES 
+                (';
+    $sql.=$parameters.')"';
+    $create.='
+            $stmt = $this->handler->prepare('.$sql.');
+            ';
+    foreach ($attributes as $attribute){
+        $create.='$stmt->bindParam(\':'.$attribute['name'].'\',$'.strtolower($class).'->get'.ucfirst($attribute['name']).'());
+            ';
+    }            
+    $create.='if($stmt->execute()){
+                $'.strtolower($class).'->setId(intval($this->handler->lastInsertID()));
+                $created=$'.strtolower($class).';
+            }else{
+                $error=$stmt->errorInfo();
+                error_log("[".__FILE__.":".__LINE__."]"."Mysql: ".$error[2]);
+            }
+        }else{
+            $created="exist";
+        }
+        return $created;
+    }';
+/************************************ READ ************************************/
+    $read='
+    /**
+     * Read a '.$class.' from the database
+     * @param int '.$class.' identificator
+     * @return '.$class.' '.$class.' loaded
+     */
+    function read($id){
+        $response=null;
+        if($this->exist($id)){';
+    $sql='"SELECT * FROM '.strtolower($class).'s WHERE id= ?"';
+    $read.='
+            $stmt = $this->handler->prepare('.$sql.');
+            ';
+    $read.='if ($stmt->execute(array($id))) {
+                $row=$stmt->fetch();
+                $'.strtolower($class).'=new '.$class.'();
+            ';
+    foreach ($attributes as $attribute){
+        $value='$row["'.$attribute['name'].'"]';
+        if(strtolower($attribute['type'])==="int"){
+            $value='intval($row["'.$attribute['name'].'"])';
+        }elseif(strtolower($attribute['type'])==="float"){
+            $value='floatval($row["'.$attribute['name'].'"])';
+        }
+        $read.='    $'.strtolower($class).'->set'.ucfirst($attribute['name']).'('.$value.');
+            ';
+    }
+    $read.='    $response=$'.strtolower($class).';
+            }else{
+                $error=$stmt->errorInfo();
+                error_log("[".__FILE__.":".__LINE__."]"."Mysql: ".$error[2]);
+            }
+        }
+        return $response;
+    }';
+/*********************************** UPDATE ***********************************/
+    $update='
+    /**
+     * Update a '.$class.' in the database
+     * @param '.$class.' '.$class.' object
+     * @return false if could\'nt update it
+     * @return true if the '.$class.' was updated
+     */
+    function update($'.strtolower($class).'){
+        $updated=false;
+        if($this->exist($'.strtolower($class).'->getId())){';
+    $columns='
+                ';
+    foreach ($attributes as $attribute){
+        if($attribute['name']!="id"){
+            $columns.='`'.$attribute['name'].'`=:'.$attribute['name'].',
+                ';
+        }
+    }
+    $columns=substr($columns,0,-1);
+    $sql='"UPDATE '.strtolower($class).'s SET '.$columns.' WHERE id=:id"';
+    $update.='
+            $stmt = $this->handler->prepare('.$sql.');
+            ';
+    foreach ($attributes as $attribute){
+        $update.='$stmt->bindParam(\':'.$attribute['name'].'\',$'.strtolower($class).'->get'.ucfirst($attribute['name']).'());
+            ';
+    }            
+    $update.='if($stmt->execute()){
+                $updated=true;
+            }else{
+                $error=$stmt->errorInfo();
+                error_log("[".__FILE__.":".__LINE__."]"."Mysql: ".$error[2]);
+            }
+        }else{
+            $updated=false;
+        }
+        return $updated;
+    }';
+/*********************************** DELETE ***********************************/    
+    $delete='
+    /**
+     * Delete an '.$class.' from the database
+     * @param '.$class.' '.$class.' object
+     * @return false if could\'nt delete it
+     * @return true if the '.$class.' was deleted
+     */
+    function delete($'.strtolower($class).'){
+        $deleted=false;
+        if($this->exist($'.strtolower($class).'->getId())){
+            $stmt = $this->handler->prepare("DELETE '.strtolower($class).'s WHERE id=:id");
+            $stmt->bindParam(\':id\',$'.strtolower($class).'->getId());
+            if($stmt->execute()){
+                $deleted=true;
+            }else{
+                $error=$stmt->errorInfo();
+                error_log("[".__FILE__.":".__LINE__."]"."Mysql: ".$error[2]);
+            }
+        }else{
+            $deleted=false;
+        }
+        return $deleted;
+    }';
+/************************************ EXIST ***********************************/    
+    $exist='
+    /**
+     * Return if a '.$class.' exist in the database
+     * @param int '.$class.' identificator
+     * @return false if doesn\'t exist
+     * @return true if exist
+     */
+    function exist($id){
+        $exist=false;
+        $stmt = $this->handler->prepare("SELECT id FROM '.strtolower($class).'s WHERE id=:id");
+        $stmt->bindParam(\':id\',$id);
+        if ($stmt->execute()) {
+            $list=$stmt->fetch();
+            if($list){
+                if(intval($list["id"])===intval($id)){
+                    $exist=true;
+                }else{
+                    $exist=false;
+                }
+            }
+        }
+        return $exist;
+    }';
+/*********************************** LISTING **********************************/    
+    $listing='
+    /**
+     * Get the list of '.$class.'
+     * @return '.$class.'[] List of '.$class.'
+     */
+    function listing(){
+        $list=array();
+        $stmt = $this->handler->prepare("SELECT id FROM '.strtolower($class).'s");
+        if ($stmt->execute()) {
+            while ($row = $stmt->fetch()){
+                $'.strtolower($class).'=$this->read($row["id"]);
+                array_push($list,$'.strtolower($class).');
+            }
+        }
+        return $list;
+    }';
+/********************************* CLASS **********************************/
+$class='
+class Dao'.$class.' extends Dao{';
+$class.=$constructor;
+$class.=$create;
+$class.=$read;
+$class.=$update;
+$class.=$delete;
+$class.=$exist;
+$class.=$listing;
+$class.='
+}
+?>';
+//Escribe la clase
+file_put_contents($file,$header.$class);
+print_r("CLASE DE MODELO GENERADA <br>");
+}
+
+
+?>
