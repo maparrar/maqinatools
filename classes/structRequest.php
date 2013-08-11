b8dc410700b00b3c2fe42747d936f2038ab9ccd3
maparrar <maparrar@gmail.com>
2013-08-11 03:41:20 -0500

    Nuevos directorios

--- classes/structRequest.php
+++
@@ -1,8 +0,0 @@
-$package="core";
-    $subpackage="routing";
-    $class="Request";
-    $attributes=array(
-        array("name"=>"controller", "type"=>"string",   "comment"=>"Controlador del request"),
-        array("name"=>"function", "type"=>"string",   "comment"=>"Función del request"),
-        array("name"=>"parameters", "type"=>"array",   "comment"=>"Parámetros pasados al request")
-    );
